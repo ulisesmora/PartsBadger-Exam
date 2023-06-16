2263916628baf054b67662502494275ff0872847
ulisem <60708239+ulisem@users.noreply.github.com>
2023-06-16 10:09:20 -0600

    fix docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
-    host: '0.0.0.0', // needed for the Docker Container port mapping to work
+    host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 3000, // you can replace this port with any port
   }
